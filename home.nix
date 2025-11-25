d2f0627180dab6729cbc08b84313fbfbf27fcada
tener <matsuda319@icloud.com>
2025-11-26 02:09:53 +0900

    change home-manager to master branch

--- home.nix
+++ home.nix
@@ -1,18 +1,19 @@
-  { pkgs, ...}
+  { pkgs, ...}:
   {
     home.username = "tener";
     home.homeDirectory = "/home/tener";
     home.stateVersion = "25.05";
-  };
 
     programs.git = {
       enable = true;
-      userName = "tenelol"
-      userEmail = "matsuda319@icloud.com"
-      extraConfig.core.editore = "nvim";
+      userName = "tenelol";
+      userEmail = "matsuda319@icloud.com";
+      extraConfig.core.editor = "nvim";
   };
 
     home.packages = with pkgs; [
       neovim
-  ];
+      hyprland
+    ];
+  }
 
