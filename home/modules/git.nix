1747dbd361f2969e987ede5eda5b0f5c6e1e0052
tener <matsuda319@icloud.com>
2025-11-30 00:07:01 +0900

    add home/

---
+++ home/modules/git.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }:
+  
+  programs.git.settings = {
+      enable = true;
+      userName = "tenelol";
+      userEmail = "matsuda319@icloud.com";
+      extraConfig.core.editor = "nvim";
+    };
