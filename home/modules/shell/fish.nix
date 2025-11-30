b993931de213ab770300488c790d42c2f5572d53
tener <matsuda319@icloud.com>
2025-11-30 12:41:46 +0900

    test: ✅ test commit

--- home/modules/shell/fish.nix
+++ home/modules/shell/fish.nix
@@ -1,18 +1,11 @@
 { config, pkgs, ... }:
 
-let
-  # 実際の fish スクリプトは別ファイルで管理
-
-in
-{
   programs.fish = {
     enable = true;
 
     interactiveShellInit = ''
-      set fish_greeting ""
-
-      # 記事で書いてた LSCOLORS
-      set -x LSCOLORS Gxfxcxdxbxegedabagacad
+     set fish_greeting ""
+     set -x LSCOLORS Gxfxcxdxbxegedabagacad
     '';
   };
 
