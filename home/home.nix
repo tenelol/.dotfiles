44b1250b13167877840dc367a4e1c36b00b8ec01
tener <matsuda319@icloud.com>
2025-11-30 12:41:46 +0900

    test: test commit

--- home/home.nix
+++ home/home.nix
@@ -6,20 +6,12 @@
     home.enableNixpkgsReleaseCheck = false;
     xdg.enable = true;
 
-
-#programs.git.settings = {
- #     enable = true;
-  #    userName = "tenelol";
-   #   userEmail = "matsuda319@icloud.com";
-    #  extraConfig.core.editor = "nvim";
-    #};
-
     home.packages = with pkgs; [
       hyprland kitty eww gh hyprpaper waybar firefox google-chrome wofi floorp-bin grim slurp wl-clipboard eza bat tre-command
     ];
 
-    import = [
-   #   ./modules/shell/fish.nix
+    imports = [
+      ./modules/shell/fish.nix
       ./modules/git.nix
     ];
 
--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,8 +1,11 @@
 { config, pkgs, ... }:
-  
+
+{
   programs.git.settings = {
       enable = true;
       userName = "tenelol";
       userEmail = "matsuda319@icloud.com";
       extraConfig.core.editor = "nvim";
     };
+
+}
