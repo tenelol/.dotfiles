50ad0ab9a5e6eb81487a0a3062c732504d49c44b
tener <matsuda319@icloud.com>
2025-11-30 13:44:52 +0900

    complete fish

--- home/modules/shell/fish.nix
+++ home/modules/shell/fish.nix
@@ -3,17 +3,20 @@
 {
   programs.fish = {
     enable = true;
-
+    
+    interactiveShellInit = ''
+      set fish_greeting "こにちは"
+    '';
     shellAliases = {
       ls = "eza --icons";
     };
   };
   
   xdg.configFile."fish/functions/fish_prompt.fish".source =
-    ../../fish/functions/fish_prompt.fish;
+    ../../../fish/functions/fish_prompt.fish;
 
   xdg.configFile."fish/functions/fish_right_prompt.fish".source =
-    ../../fish/functions/fish_right_prompt.fish;
+    ../../../fish/functions/fish_right_prompt.fish;
 
 }
 
