ae4d0d65b320301c764c91e5b26736f5befa0981
tener <matsuda319@icloud.com>
2025-11-30 13:14:16 +0900

    feat: ✨ add fish.conf

--- home/modules/shell/fish.nix
+++ home/modules/shell/fish.nix
@@ -1,14 +1,19 @@
 { config, pkgs, ... }:
 
+{
   programs.fish = {
     enable = true;
 
-    interactiveShellInit = ''
-     set fish_greeting ""
-     set -x LSCOLORS Gxfxcxdxbxegedabagacad
-    '';
+    shellAliases = {
+      ls = "eza --icons";
+    };
   };
+  
+  xdg.configFile."fish/functions/fish_prompt.fish".source =
+    ../../fish/functions/fish_prompt.fish;
 
+  xdg.configFile."fish/functions/fish_right_prompt.fish".source =
+    ../../fish/functions/fish_right_prompt.fish;
 
 }
 
