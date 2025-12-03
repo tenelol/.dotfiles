e44c16de8023456ebb73a1e5da5724dfa246897b
tener <matsuda319@icloud.com>
2025-12-03 13:43:43 +0900

    add nvim.nix

--- nvim/lua/plugins/language-server.lua
+++ nvim/lua/plugins/language-server.lua
@@ -1,5 +1,20 @@
 return {
-  "neovim/nvim-lspconfig",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local servers = { "lua_ls", "pyright", "gopls", "nil_ls" }
+
+      for _, server in ipairs(servers) do
+        vim.lsp.config(server, {
+          capabilities = capabilities,
+        })
+      end
+
+      vim.lsp.enable(servers)
+    end,
+  },
 }
+
