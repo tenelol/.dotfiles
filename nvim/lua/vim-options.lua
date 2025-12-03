98b2bab7560686c20eae687beb34726523727bc7
tener <matsuda319@icloud.com>
2025-12-03 11:37:42 +0900

    add nvim section

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,19 @@
+-- leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--keymap
+vim.keymap.set("i", "kj", "<ESC>", { silent = true })
+vim.keymap.set('t', '<C-s>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+vim.keymap.set("t", "<C-h>", "<Cmd>wincmd h<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-j>", "<Cmd>wincmd j<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-k>", "<Cmd>wincmd k<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-l>", "<Cmd>wincmd l<CR>", { noremap = true, silent = true })
+
+vim.opt.clipboard = "unnamedplus"
