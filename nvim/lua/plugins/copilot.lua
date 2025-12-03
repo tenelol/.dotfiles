f37ceda268b95f7fc778aed00058239691602bfe
tener <matsuda319@icloud.com>
2025-12-03 17:08:57 +0900

    feat: ✨ add copilot.vim

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  "github/copilot.vim",
+  event = "InsertEnter",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', {
+      silent = true,
+      expr = true,
+    })
+  end,
+}
+
