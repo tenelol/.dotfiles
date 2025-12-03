2a374ef9f5794d533dd5ae2d6a311ec133c415b9
tener <matsuda319@icloud.com>
2025-12-03 17:08:57 +0900

    add copilot.vim

--- nvim/lua/plugins/winresizer.lua
+++ nvim/lua/plugins/winresizer.lua
@@ -1,4 +1,11 @@
-{
+return {
   "simeji/winresizer",
+
+  event = "VeryLazy",
+
+  config = function()
+
+    vim.keymap.set("n", "<C-w>e", ":WinResizerStartResize<CR>", { silent = true })
+  end,
 }
 
