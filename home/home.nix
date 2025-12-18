4fa28b4424717620c3c29370e4f659612cd14607
tener <matsuda319@icloud.com>
2025-12-18 11:17:59 +0900

    chore: 🧹 change directory, niri.nix

--- home/home.nix
+++ home/home.nix
@@ -1,26 +1,35 @@
-  { pkgs, ...}:
-  {
-    home.username = "tener";
-    home.homeDirectory = "/home/tener";
-    home.stateVersion = "25.05";
-    home.enableNixpkgsReleaseCheck = false;
+{ config, pkgs, lib, ... }:
 
-    home.packages = with pkgs; [
-      kitty gh waybar parted
-      wofi floorp-bin sqlitebrowser
-      eza bat fuzzel tre-command imv ripgrep
-      incus zstd
-    ];
+let
+  host = config.networking.hostName or "";
+in
+{
+  home.username = "tener";
+  home.homeDirectory = "/home/tener";
+  home.stateVersion = "25.05";
+  home.enableNixpkgsReleaseCheck = false;
 
-    imports = [
+  home.packages = with pkgs; [
+    kitty gh waybar parted
+    wofi floorp-bin sqlitebrowser
+    eza bat fuzzel tre-command imv ripgrep
+    incus zstd
+  ];
+
+  imports =
+    [
       ./modules/shell/fish.nix
       ./modules/kitty.nix
       ./modules/git.nix
       ./modules/nvim.nix
       ./modules/hypr.nix
       ./modules/waybar.nix
-      ./modules/niri.nix       
+    ]
+    ++ lib.optionals (host == "nvidia-desktop") [
+      ./hosts/nvidia-desktop/niri.nix
+    ]
+    ++ lib.optionals (host == "nixos") [
+      ./hosts/nixos/niri.nix
     ];
- 
+}
 
- }
