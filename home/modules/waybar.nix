dd19e45e600cce132a2e314322ee4ceb6afb573b
tenelol <matsuda319@icloud.com>
2025-12-29 01:06:58 +0900

    change waybar2

--- home/modules/waybar.nix
+++ home/modules/waybar.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  xdg.configFile."waybar".source = ../../config/waybar;
+  xdg.configFile."waybar".source = ../../config/waybar2;
 
   home.packages = with pkgs; [
     waybar
