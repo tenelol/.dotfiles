d60cf39523808d165cd338f7633d99cec7daf8b7
tenelol <matsuda319@icloud.com>
2026-01-07 18:59:43 +0900

    fix: 🐛 fix term in niri

--- hosts/nvidia-desktop/nvidia-desktop.nix
+++ hosts/nvidia-desktop/nvidia-desktop.nix
@@ -1,13 +1,2 @@
-{ config, lib, pkgs, osConfig, ... }:
-let
-  isDesktop = (osConfig.networking.hostName or "") == "nvidia-desktop";
-in
-{
-  config = lib.mkIf isDesktop {
-    xdg.configFile."niri/config.kdl".text = ''
-      Mod+T hotkey-overlay-title="Open a Terminal: ghostty" { spawn "ghostty"; }
-    '';
-    home.packages = with pkgs; [ ghostty ];
-  };
-}
-
+{ ... }:
+{ }
