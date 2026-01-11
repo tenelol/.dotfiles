169e963997eb741f633031c9847331376b55a8a1
tener <tenelol@tenelol.dev>
2026-01-12 03:26:57 +0900

    style: 💄 Update fish greetings

--- home/modules/shell/fish.nix
+++ home/modules/shell/fish.nix
@@ -3,21 +3,21 @@
 {
   programs.fish = {
     enable = true;
-    
-  interactiveShellInit = ''
-    set -g greetings "🥳Hollow World!🥳" "こにちは" "酒!暴力!Linux!"
-    set -l count (count $greetings)
-    set -l r (random)
-    set -l idx (math "$r % $count + 1")
-    set fish_greeting $greetings[$idx]
-'';
+
+    interactiveShellInit = ''
+      set -g greetings "🥳Hollow World!🥳" "👏Welcome back!👏" "🚀Ready to code?🚀" "💡Let's be productive!💡" "💰Time is money!💰" "🔥Stay Hungry!🔥"
+      set -l count (count $greetings)
+      set -l r (random)
+      set -l idx (math "$r % $count + 1")
+      set fish_greeting $greetings[$idx]
+    '';
 
     shellAliases = {
       ls = "eza --icons";
       deploy-portfolio = "ssh -t homeserver '~/bin/deploy-portfolio.sh'";
     };
   };
-  
+
   xdg.configFile."fish/functions/fish_prompt.fish".source =
     ../../../config/fish/functions/fish_prompt.fish;
 
@@ -25,4 +25,3 @@
     ../../../config/fish/functions/fish_right_prompt.fish;
 
 }
-
