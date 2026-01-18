d78099174d4b7007563f0cee1857488fe5bcfb89
tenelol <matsuda319@icloud.com>
2026-01-19 02:40:54 +0900

    feat: Set Zen as default browser

--- home/modules/zen-browser.nix
+++ home/modules/zen-browser.nix
@@ -7,4 +7,11 @@ in
   home.packages = [
     zenPackages.default
   ];
+
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = [ "zen-beta.desktop" ];
+    "application/xhtml+xml" = [ "zen-beta.desktop" ];
+    "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+    "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+  };
 }
