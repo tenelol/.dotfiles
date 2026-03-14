5f9a9d76966b6f802cecf6cf28a9a4682d624411
tenelol <matsuda319@icloud.com>
2026-03-15 03:30:32 +0900

    fix: Set secondary monitor to 165Hz

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -26,7 +26,7 @@ delib.module {
       shellAliases = {
         ls = "eza --icons";
         niri-mirror = "niri msg output DP-3 on; niri msg output HDMI-A-1 on; niri msg output DP-3 mode 1920x1080@59.934; niri msg output HDMI-A-1 mode 1920x1080@59.934; niri msg output DP-3 scale 1.0; niri msg output HDMI-A-1 scale 1.0; niri msg output DP-3 position set 0 0; niri msg output HDMI-A-1 position set 0 0";
-        niri-extend = "niri msg output DP-3 on; niri msg output HDMI-A-1 on; niri msg output DP-3 mode 3840x2160@60.000; niri msg output HDMI-A-1 mode 1920x1080@59.934; niri msg output DP-3 scale 1.5; niri msg output HDMI-A-1 scale 1.0; niri msg output DP-3 position set 0 0; niri msg output HDMI-A-1 position set 2560 0";
+        niri-extend = "niri msg output DP-3 on; niri msg output HDMI-A-1 on; niri msg output DP-3 mode 3840x2160@60.000; niri msg output HDMI-A-1 mode 1920x1080@165.000; niri msg output DP-3 scale 1.5; niri msg output HDMI-A-1 scale 1.0; niri msg output DP-3 position set 0 0; niri msg output HDMI-A-1 position set 2560 0";
       };
     };
 
