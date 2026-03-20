fd1393dabd1809fe971c19d9b80e6eda7edd6b43
tener <tenelol@tenelol.dev>
2026-03-20 23:08:12 +0900

    feat: Add VSCode-like navigation to Neovim

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,16 @@
 local plugin = require("nix-plugin")
 
 return {
-  plugin.spec("telescope-nvim", {
-  dependencies = { plugin.dep("plenary-nvim") },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<C-b>", builtin.find_files, { desc = "Find files" })
-  end,
-  }),
+    plugin.spec("telescope-nvim", {
+        dependencies = { plugin.dep("plenary-nvim") },
+        config = function()
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Quick open" })
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Search in files" })
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+            vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+        end,
+    }),
 }
--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -1,20 +1,23 @@
 local plugin = require("nix-plugin")
 
 return {
-  plugin.spec("which-key-nvim", {
-    event = "VeryLazy",
-    config = function()
-      local wk = require("which-key")
+    plugin.spec("which-key-nvim", {
+        event = "VeryLazy",
+        config = function()
+            local wk = require("which-key")
 
-      wk.setup({
-        delay = 200,
-      })
+            wk.setup({
+                delay = 200,
+            })
 
-      wk.add({
-        { "<leader>d", group = "debug" },
-        { "<leader>h", group = "git hunk" },
-        { "<leader>t", group = "test" },
-      })
-    end,
-  }),
+            wk.add({
+                { "<leader>f", group = "find" },
+                { "<leader>d", group = "debug" },
+                { "<leader>h", group = "git hunk" },
+                { "<leader>l", group = "language" },
+                { "<leader>x", group = "problems" },
+                { "<leader>t", group = "test" },
+            })
+        end,
+    }),
 }
