b67999228ebe73f4efc2c8ed8b666c03befb5a7e
tenelol <tenelol@tenelol.dev>
2026-04-14 13:13:57 +0900

    fix: Load Copilot command lazily

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,19 +1,20 @@
 local plugin = require("nix-plugin")
 
 return {
-  plugin.spec("copilot-vim", {
-  event = "InsertEnter",
-  config = function()
-    vim.g.copilot_no_tab_map = true
+    plugin.spec("copilot-vim", {
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            vim.g.copilot_no_tab_map = true
 
-    vim.keymap.set("i", "<C-l>", function()
-      return vim.fn["copilot#Accept"]("<CR>")
-    end, {
-      silent = true,
-      expr = true,
-      replace_keycodes = false,
-      desc = "Accept Copilot suggestion",
-    })
-  end,
-  }),
+            vim.keymap.set("i", "<C-l>", function()
+                return vim.fn["copilot#Accept"]("<CR>")
+            end, {
+                silent = true,
+                expr = true,
+                replace_keycodes = false,
+                desc = "Accept Copilot suggestion",
+            })
+        end,
+    }),
 }
