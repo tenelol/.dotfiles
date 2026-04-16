fda40d8daac9621d23c63bde6e1503468da61cc5
tenelol <tenelol@tenelol.dev>
2026-04-16 13:37:33 +0900

    feat: ✨ Add flexible Git identity includes

--- modules/git.nix
+++ modules/git.nix
@@ -11,6 +11,15 @@ delib.module {
   home.always = {
     programs.git = {
       enable = true;
+      includes = [
+        {
+          path = "~/.config/git/config.local";
+        }
+        {
+          condition = "gitdir:~/Documents/SW-exercise1/";
+          path = "~/.config/git/school";
+        }
+      ];
       settings = {
         user.name = profile.gitName;
         user.email = profile.gitEmail;
