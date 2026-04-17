45af0ec36d99fe256696f56935a45413200a5eed
tenelol <tenelol@tenelol.dev>
2026-04-17 19:47:27 +0900

    feat: ✨ Add iniad-commit package

---
+++ packages/iniad-commit.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  lib,
+}:
+pkgs.stdenvNoCC.mkDerivation {
+  pname = "iniad-commit";
+  version = "0.1.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "tenelol";
+    repo = "iniad-commit";
+    rev = "v0.1.0";
+    hash = "sha256-iJl0fzqfC/Pua2bPnA23pxsDxbuqQvpA5qSYESEdk7Q=";
+  };
+
+  nativeBuildInputs = [
+    pkgs.makeWrapper
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 git-iniad-commit-msg $out/libexec/git-iniad-commit-msg
+    install -Dm755 git-auto-commit $out/bin/git-auto-commit
+    patchShebangs $out/libexec/git-iniad-commit-msg
+    patchShebangs $out/bin/git-auto-commit
+
+    wrapProgram $out/bin/git-auto-commit \
+      --set GIT_AUTO_COMMIT_PYTHON ${pkgs.python3}/bin/python3 \
+      --set GIT_AUTO_COMMIT_SCRIPT $out/libexec/git-iniad-commit-msg \
+      --prefix PATH : ${lib.makeBinPath [ pkgs.git ]}
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Git subcommand that generates commit messages with the INIAD OpenAI API";
+    homepage = "https://github.com/tenelol/iniad-commit";
+    license = lib.licenses.mit;
+    mainProgram = "git-auto-commit";
+    platforms = lib.platforms.all;
+  };
+}
