f9a838223dc63aee23858c1d46c58aaaa4b63ef5
tenelol <tenelol@tenelol.dev>
2026-05-07 03:08:53 +0900

    feat: Enable NotchMusic autostart

--- home/home.nix
+++ home/home.nix
@@ -21,9 +21,6 @@ let
   moocsCollectPackage = import ../packages/moocs-collect.nix {
     inherit pkgs lib;
   };
-  notchMusicPackage = import ../packages/notchmusic.nix {
-    inherit pkgs lib;
-  };
   iniadCommitPackage = import ../packages/iniad-commit.nix {
     inherit pkgs lib;
   };
@@ -108,7 +105,6 @@ let
   darwinDesktopPackages = with pkgs; [
     google-chrome
     moocsCollectPackage
-    notchMusicPackage
     sqlitebrowser
     unicode-emoji
     obsidian
