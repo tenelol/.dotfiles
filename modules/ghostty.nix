4818058582b734f46a545772e8724ba575959674
tenelol <tenelol@tenelol.dev>
2026-05-14 22:27:31 +0900

    style: 💄 Use Ghostty ripple rectangle

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -38,21 +38,21 @@ let
           --replace-fail 'const float DURATION = 0.09;' 'const float DURATION = 0.13;' \
           --replace-fail 'const float THRESHOLD_MIN_DISTANCE = 1.5;' 'const float THRESHOLD_MIN_DISTANCE = 1.0;'
       '';
-  ghosttyRippleCursorShader =
-    pkgs.runCommand "ghostty-ripple-cursor-subtle.glsl"
+  ghosttyRippleRectangleCursorShader =
+    pkgs.runCommand "ghostty-ripple-rectangle-cursor-subtle.glsl"
       {
         src = pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/sahaj-b/ghostty-cursor-shaders/06d4e90fb5410e9c4d0b3131584060adddf89406/ripple_cursor.glsl";
-          hash = "sha256-QQhm2WeC3AfHDPMcWLh0DMItug54F/6+7fcW3sTYuIQ=";
+          url = "https://raw.githubusercontent.com/sahaj-b/ghostty-cursor-shaders/06d4e90fb5410e9c4d0b3131584060adddf89406/ripple_rectangle_cursor.glsl";
+          hash = "sha256-KnrJqjKjyxNku33b2eYHCSquwKzibVsPO8pB5xxugMg=";
         };
       }
       ''
         cp "$src" "$out"
         substituteInPlace "$out" \
-          --replace-fail 'const float MAX_RADIUS = 0.05;' 'const float MAX_RADIUS = 0.026;' \
+          --replace-fail 'const float MAX_SIZE = 0.05;' 'const float MAX_SIZE = 0.026;' \
           --replace-fail 'const float RING_THICKNESS = 0.02;' 'const float RING_THICKNESS = 0.014;' \
           --replace-fail 'vec4 COLOR = vec4(0.35, 0.36, 0.44, 1.0);' 'vec4 COLOR = vec4(0.35, 0.36, 0.44, 0.45);' \
-          --replace-fail 'const float BLUR = 3.0;' 'const float BLUR = 3.5;'
+          --replace-fail 'const float BLUR = 1.0;' 'const float BLUR = 1.5;'
         awk '
           /^    \/\/ Normalization & setup \(-1 to 1 coords\)/ {
             print "    if (iFocus == 0) {";
@@ -75,7 +75,7 @@ delib.module {
       "ghostty/config".source = ghosttyConfig;
       "ghostty/shaders/aurora.glsl".source = ghosttyAuroraShader;
       "ghostty/shaders/cursor_tail.glsl".source = ghosttyCursorTailShader;
-      "ghostty/shaders/ripple_cursor.glsl".source = ghosttyRippleCursorShader;
+      "ghostty/shaders/ripple_rectangle_cursor.glsl".source = ghosttyRippleRectangleCursorShader;
     };
   };
 }
