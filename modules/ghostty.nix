23c8996c47a2513b60c9bbff1aa2811cb79a3f24
tenelol <tenelol@tenelol.dev>
2026-05-14 20:13:46 +0900

    fix: 🐛 Disable Ghostty cursor animation

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -17,10 +17,6 @@ delib.module {
   options = delib.singleEnableOption (!host.isServer);
 
   home.ifEnabled = {
-    xdg.configFile = {
-      "ghostty/config".source = ghosttyConfig;
-      "ghostty/shaders/cursor-smear.glsl".source =
-        ../config/ghostty/shaders/cursor-smear.glsl;
-    };
+    xdg.configFile."ghostty/config".source = ghosttyConfig;
   };
 }
