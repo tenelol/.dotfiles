c3bc6819f600785ba80b76dddce56bb5ddc5d138
tenelol <tenelol@tenelol.dev>
2026-05-16 17:08:29 +0900

    feat: ✨ Use dark gradients for window borders

--- modules/jankyborders.nix
+++ modules/jankyborders.nix
@@ -23,9 +23,10 @@ delib.module {
           "style=round"
           "width=4.0"
           "hidpi=on"
-          "active_color=gradient(top_left=0xff7e22ce,bottom_right=0xff0e7490)"
-          "inactive_color=gradient(top_left=0xff3b0764,bottom_right=0xff164e63)"
+          "active_color=gradient(top_left=0xff0f172a,bottom_right=0xff334155)"
+          "inactive_color=gradient(top_left=0xff020617,bottom_right=0xff1e293b)"
           "background_color=0x00000000"
+          "blacklist=Codex"
         ];
         RunAtLoad = true;
         KeepAlive = true;
