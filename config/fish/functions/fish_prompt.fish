693d881c65a1aa5460baef26b73d1ea7eea01176
tenelol <tenelol@tenelol.dev>
2026-05-16 18:07:43 +0900

    style: Unify terminal dark theme

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,14 +1,20 @@
 function fish_prompt
+        set -l tn_fg c0caf5
+        set -l tn_blue 7aa2f7
+        set -l tn_cyan 7dcfff
+        set -l tn_red f7768e
+        set -l tn_yellow e0af68
+
         if test -n "$SSH_TTY"
-                echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+                echo -n (set_color $tn_red)"$USER"(set_color $tn_fg)'@'(set_color $tn_yellow)(prompt_hostname)' '
         end
-    
-        echo -n (set_color blue)(prompt_pwd)' '
-    
-        set_color -o
+
+        echo -n (set_color $tn_blue)(prompt_pwd)' '
+
         if fish_is_root_user
-                echo -n (set_color red)'# '
+                echo -n (set_color --bold $tn_red)'# '
         end
-        echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+
+        echo -n (set_color --bold $tn_cyan)'❯ '
         set_color normal
 end
