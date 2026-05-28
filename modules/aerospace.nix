528201c432111308233f0d8ccb41f2dea4fd6284
tenelol <tenelol@tenelol.dev>
2026-05-28 16:29:42 +0900

    fix: 🐛 Stabilize macOS WM handoff

--- modules/aerospace.nix
+++ modules/aerospace.nix
@@ -52,10 +52,6 @@ delib.module {
       managedBy = "aerospace";
     };
 
-    # Disable "Displays have separate Spaces" for AeroSpace. Separate macOS
-    # Spaces can make same-app windows on different monitors raise together.
-    system.defaults.spaces.spans-displays = lib.mkForce true;
-
     system.activationScripts.stopRiftForAerospace.text = ''
       uid="$(/usr/bin/id -u ${profile.username})"
 
