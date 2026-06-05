56e51d82fdd5bf75c626dd25c93990ea20f83b7d
tenelol <tenelol@tenelol.dev>
2026-06-05 21:01:18 +0900

    fix: 🐛 Use Karabiner for left Command IME toggle

--- modules/karabiner.nix
+++ modules/karabiner.nix
@@ -11,7 +11,7 @@ in
 delib.module {
   name = "karabiner";
 
-  options = delib.singleEnableOption false;
+  options = delib.singleEnableOption isDarwinDesktop;
 
   darwin.ifEnabled = lib.mkIf isDarwinDesktop {
     homebrew.casks = [
