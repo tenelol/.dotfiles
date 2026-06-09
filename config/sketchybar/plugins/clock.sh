8a2e98d710da2a022b71a7a324bf68cb0f7c9d47
tenelol <tenelol@tenelol.dev>
2026-06-09 18:18:29 +0900

    style: 💄 Simplify SketchyBar workspace display

--- config/sketchybar/plugins/clock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-SKETCHYBAR_BIN="/opt/homebrew/bin/sketchybar"
-clock_text="$(LC_TIME=en_US.UTF-8 LANG=en_US.UTF-8 /bin/date '+%a %b %-d %H:%M')"
-
-"$SKETCHYBAR_BIN" --set "$NAME" label="$clock_text"
