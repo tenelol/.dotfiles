ffb6bd2c5ab8ff4b2b7b24f20568eacdc42c898e
tenelol <tenelol@tenelol.dev>
2026-06-13 01:12:48 +0900

    fix: 🐛 Preserve WSL Neovim input keys

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -4,6 +4,7 @@ vim.g.maplocalleader = " "
 local map = vim.keymap.set
 local cmd = vim.cmd
 local select_all = "<Esc>ggVG"
+local is_wsl = vim.fn.has("wsl") == 1
 
 vim.api.nvim_create_user_command("SelectAll", "normal! ggVG", {
   desc = "Select the entire buffer",
@@ -17,10 +18,17 @@ if vim.fn.has("mac") == 1 then
   map({ "n", "i", "v" }, "<D-a>", select_all, { silent = true, desc = "Select all" })
 end
 
-map("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-map("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
-map("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
-map("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+if is_wsl then
+  map({ "n", "i", "v" }, "<C-h>", "<Left>", { noremap = true, silent = true, desc = "Move left" })
+  map({ "n", "i", "v" }, "<C-j>", "<Down>", { noremap = true, silent = true, desc = "Move down" })
+  map({ "n", "i", "v" }, "<C-k>", "<Up>", { noremap = true, silent = true, desc = "Move up" })
+  map({ "n", "i", "v" }, "<C-l>", "<Right>", { noremap = true, silent = true, desc = "Move right" })
+else
+  map("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+  map("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+  map("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+  map("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+end
 
 map("n", "K", function()
   for _, client in ipairs(vim.lsp.get_clients({ bufnr = 0 })) do
@@ -37,10 +45,12 @@ map("n", "K", function()
   end
 end, { silent = true, desc = "Hover" })
 
-map("t", "<C-h>", "<Cmd>wincmd h<CR>", { noremap = true, silent = true })
-map("t", "<C-j>", "<Cmd>wincmd j<CR>", { noremap = true, silent = true })
-map("t", "<C-k>", "<Cmd>wincmd k<CR>", { noremap = true, silent = true })
-map("t", "<C-l>", "<Cmd>wincmd l<CR>", { noremap = true, silent = true })
+if not is_wsl then
+  map("t", "<C-h>", "<Cmd>wincmd h<CR>", { noremap = true, silent = true })
+  map("t", "<C-j>", "<Cmd>wincmd j<CR>", { noremap = true, silent = true })
+  map("t", "<C-k>", "<Cmd>wincmd k<CR>", { noremap = true, silent = true })
+  map("t", "<C-l>", "<Cmd>wincmd l<CR>", { noremap = true, silent = true })
+end
 
 map("n", "<C-Tab>", "<Cmd>BufferLineCycleNext<CR>", { silent = true, desc = "Next buffer" })
 map("n", "<C-S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { silent = true, desc = "Previous buffer" })
