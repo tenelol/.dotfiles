0c652bba9e639396afc6d26c2ba123fda00aa872
tenelol <tenelol@tenelol.dev>
2026-07-01 14:39:45 +0900

    refactor: ♻️ Extract Hazkey package overrides

--- modules/nixos-hazkey.nix
+++ modules/nixos-hazkey.nix
@@ -7,93 +7,12 @@
   ...
 }:
 let
-  nix-hazkey = inputs.nix-hazkey;
-  inherit (pkgs.stdenv.hostPlatform) system;
-  hazkeyHosts = [
-    "nixos"
-    "nvidia-desktop"
-  ];
-  hazkeyEnabled = builtins.elem host.name hazkeyHosts;
-  hasHazkeyPackages = builtins.hasAttr system nix-hazkey.packages;
-  hasX86_64Binary = system == "x86_64-linux";
-  isSupportedHazkeySystem = hasHazkeyPackages && hasX86_64Binary;
-
-  hazkeyVersion = "0.2.1";
-  hazkeySrc =
-    if hasX86_64Binary then
-      pkgs.fetchzip {
-        name = "fcitx5-hazkey-bin";
-        version = hazkeyVersion;
-        urls = [
-          "https://github.com/7ka-Hiira/fcitx5-hazkey/releases/download/${hazkeyVersion}/fcitx5-hazkey-${hazkeyVersion}-x86_64.tar.gz"
-          "https://ghproxy.net/https://github.com/7ka-Hiira/fcitx5-hazkey/releases/download/${hazkeyVersion}/fcitx5-hazkey-${hazkeyVersion}-x86_64.tar.gz"
-          "https://github.moeyy.xyz/https://github.com/7ka-Hiira/fcitx5-hazkey/releases/download/${hazkeyVersion}/fcitx5-hazkey-${hazkeyVersion}-x86_64.tar.gz"
-        ];
-        hash = "sha256-jwv1UTRz/FVHmeaumwP45Q4JZcSuZHTrF2/PAzrxeC8=";
-        stripRoot = false;
-      }
-    else
-      null;
-
-  hazkeyPackages = if hasHazkeyPackages then nix-hazkey.packages.${system} else null;
-  fcitx5Hazkey =
-    if isSupportedHazkeySystem then
-      hazkeyPackages.fcitx5-hazkey.overrideAttrs (_: {
-        src = hazkeySrc;
-      })
-    else
-      null;
-  hazkeySettings =
-    if isSupportedHazkeySystem then
-      hazkeyPackages.hazkey-settings.overrideAttrs (_: {
-        src = hazkeySrc;
-      })
-    else
-      null;
-  hazkeyServer =
-    if isSupportedHazkeySystem then
-      hazkeyPackages.hazkey-server.overrideAttrs (_: {
-        src = hazkeySrc;
-      })
-    else
-      null;
-  hazkeyDictionary =
-    if isSupportedHazkeySystem then
-      hazkeyPackages.dictionary.overrideAttrs (_: {
-        src = hazkeySrc;
-      })
-    else
-      null;
-  hasLibllamaCpu = isSupportedHazkeySystem && builtins.hasAttr "libllama-cpu" hazkeyPackages;
-
-  libllamaVersion = "20251109.0";
-  libllamaSrc =
-    if hasLibllamaCpu then
-      pkgs.fetchzip {
-        name = "libllama-cpu-bin";
-        version = libllamaVersion;
-        urls = [
-          "https://github.com/7ka-Hiira/llama.cpp/releases/download/v${libllamaVersion}/llama-linux-x86_64-cpu-v${libllamaVersion}.tar.gz"
-          "https://ghproxy.net/https://github.com/7ka-Hiira/llama.cpp/releases/download/v${libllamaVersion}/llama-linux-x86_64-cpu-v${libllamaVersion}.tar.gz"
-          "https://github.moeyy.xyz/https://github.com/7ka-Hiira/llama.cpp/releases/download/v${libllamaVersion}/llama-linux-x86_64-cpu-v${libllamaVersion}.tar.gz"
-        ];
-        hash = "sha256-Hw96OYrd3LoePFhNk3Whk90I0pREx2gpxanIMxo+bHs=";
-        stripRoot = false;
-      }
-    else
-      null;
-  libllamaCpu =
-    if hasLibllamaCpu then
-      hazkeyPackages.libllama-cpu.overrideAttrs (_: {
-        src = libllamaSrc;
-      })
-    else
-      null;
+  hazkey = import ../lib/nixos/hazkey.nix { inherit host inputs pkgs; };
 in
 delib.module {
   name = "nixos.hazkey";
 
-  options = delib.singleEnableOption (hazkeyEnabled && isSupportedHazkeySystem);
+  options = delib.singleEnableOption (hazkey.enabled && hazkey.isSupportedSystem);
 
   nixos.always =
     { ... }:
@@ -101,10 +20,10 @@ delib.module {
       warnings =
         lib.optional
           (
-            hazkeyEnabled
+            hazkey.enabled
             && !host.isServer
             && builtins.match ".*-linux" host.system != null
-            && !isSupportedHazkeySystem
+            && !hazkey.isSupportedSystem
           )
           "nixos.hazkey is enabled for ${host.name}, but the pinned upstream binary overrides are only packaged for x86_64-linux.";
     };
@@ -117,7 +36,7 @@ delib.module {
         waylandFrontend = true;
         addons = with pkgs; [
           fcitx5-skk
-          fcitx5Hazkey
+          hazkey.fcitx5Addon
         ];
       };
     };
@@ -128,12 +47,12 @@ delib.module {
       enable = true;
       installFcitx5Addon = false;
       installHazkeySettings = false;
-      server.package = hazkeyServer;
-      libllama.package = libllamaCpu;
-      dictionary.package = hazkeyDictionary;
-      zenzai.package = hazkeyPackages.zenzai_v3_1-small;
+      server.package = hazkey.server;
+      libllama.package = hazkey.libllamaCpu;
+      dictionary.package = hazkey.dictionary;
+      zenzai.package = hazkey.packages.zenzai_v3_1-small;
     };
 
-    environment.systemPackages = [ hazkeySettings ];
+    environment.systemPackages = [ hazkey.settings ];
   };
 }
