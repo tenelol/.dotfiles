309025b3dbb36cd6cf7b86d1ba788f1ba2cf0002
tenelol <tenelol@tenelol.dev>
2026-07-28 10:37:53 +0900

    fix: Build AutoRaise on macOS 26

--- modules/autoraise.nix
+++ modules/autoraise.nix
@@ -9,6 +9,29 @@
 let
   homeDir = "/Users/${profile.username}";
   isDarwinDesktop = !host.isServer && builtins.match ".*-darwin" host.system != null;
+  autoraisePackage =
+    if host.name == "macbook" then
+      pkgs.autoraise.overrideAttrs (_: {
+        # cctools-binutils from Nix crashes on macOS 26 while linking
+        # AutoRaise. Use the working system toolchain for this small
+        # macOS-only GUI utility instead.
+        buildPhase = ''
+          runHook preBuild
+          /usr/bin/env -u DEVELOPER_DIR -u SDKROOT PATH=/usr/bin:/bin \
+            /Library/Developer/CommandLineTools/usr/bin/clang++ \
+            -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
+            -std=c++03 -fobjc-arc \
+            '-DNS_FORMAT_ARGUMENT(A)=' \
+            '-DSKYLIGHT_AVAILABLE=1' \
+            -F/System/Library/PrivateFrameworks \
+            -o AutoRaise AutoRaise.mm \
+            -framework AppKit -framework SkyLight
+          bash create-app-bundle.sh
+          runHook postBuild
+        '';
+      })
+    else
+      pkgs.autoraise;
 in
 delib.module {
   name = "autoraise";
@@ -19,7 +42,7 @@ delib.module {
     launchd.user.agents.autoraise = {
       serviceConfig = {
         ProgramArguments = [
-          "${pkgs.autoraise}/bin/autoraise"
+          "${autoraisePackage}/bin/autoraise"
         ];
         RunAtLoad = true;
         KeepAlive = true;
@@ -35,7 +58,7 @@ delib.module {
   };
 
   home.ifEnabled = lib.mkIf isDarwinDesktop {
-    home.packages = [ pkgs.autoraise ];
+    home.packages = [ autoraisePackage ];
 
     xdg.configFile."AutoRaise/config".source = ../config/autoraise/config;
   };
