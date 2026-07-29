20e75a9fa10e2332650e2c75aee5dd5eadc41c8f
tenelol <tenelol@tenelol.dev>
2026-07-30 04:00:23 +0900

    style: 💄 Optimize narrow TUI layouts

---
+++ modules/lazygit.nix
@@ -0,0 +1,19 @@
+{
+  delib,
+  lib,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "lazygit";
+
+  home.always = {
+    home.packages = lib.optionals pkgs.stdenv.hostPlatform.isLinux [ pkgs.lazygit ];
+  }
+  // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+    xdg.configFile."lazygit/config.yml".source = ../config/lazygit/config.yml;
+  }
+  // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
+    home.file."Library/Application Support/lazygit/config.yml".source = ../config/lazygit/config.yml;
+  };
+}
